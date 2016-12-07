7388091159a1d26b79f4d59f81386e1cbb89d51b
José Pedro Teixeira Monteiro <jpedrotm@github.com>
2016-12-07 15:01:28 +0000

    initial page completed

---
+++ pages/principal_page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Cá estámos !
+    </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+</head>
+
+<body>
+    <div id="top-bar">
+        <h1>
+            Cá estámos !
+        </h1>
+
+        <div id="login">
+            <form action="sign_in.php" method="post">
+                <input type="submit" name="submit" value="Iniciar Sessão">
+            </form>
+        </div>
+    </div>
+
+    <div id="middle">
+        <img src="../res/backgroundPhoto.jpg"/>
+        <h1>
+            Cá estámos !
+        </h1>
+        <div id="search-bar">
+            <form action="" method="post">
+                <input id="searchLocal" type="text" name="local_search" placeholder="Pesquisar por local..."/>
+                <input id="searchRestaurant" type="text" name="restaurant_search" placeholder="Pesquisar por restaurante..."/>
+                <input type="submit" value="">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>

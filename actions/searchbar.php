8c1496b2123b8f55a7aa8b8e2b7365c1be8164a8
Katchau <joaohccl@live.com.pt>
2016-12-14 17:39:39 +0000

    Fixing Stuff. Everything is going to change :alarm_clock:

--- actions/searchbar.php
+++
@@ -1,9 +0,0 @@
-<div id="searchBar">
-	<p><b>Introduza o nome do restaurante que deseja</b></p>
-	<form method="post"> 
-		Local <input type="text" id="local_rest" >
-		Nome <input type="text" id="searchbar" >
-		<ul id="txtHint">
-		</ul>
-	</form>
-</div>
